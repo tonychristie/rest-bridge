05a0eceec1789bf6e8b666cd6801f59ea71b62ee
Tony Christie <tony.christie@spirecentral.com>
2026-01-12 19:41:18 +0000

    refactor: rename package from com.spire to com.spirecentral
    Prepare for public release by updating the package namespace:
    - Rename directory structure (main and test)
    - Update all package declarations and imports
    - Update pom.xml groupId
    - Update logging configuration in application.yml

---
+++ src/main/java/com/spirecentral/restbridge/dto/UpdateObjectRequest.java
@@ -0,0 +1,26 @@
+package com.spirecentral.restbridge.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * Request to update an object's attributes.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateObjectRequest {
+
+    @NotBlank(message = "Session ID is required")
+    private String sessionId;
+
+    @NotEmpty(message = "Attributes are required")
+    private Map<String, Object> attributes;
+}
